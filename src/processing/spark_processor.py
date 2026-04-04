59890a61d1ec1bab04d8fad475ed1127fadda721
Bryan-Julius <Bryanjulius77@gmail.com>
2026-04-04 14:25:14 -0400

    feat: implement Spark DataFrames to clean HURDAT2 data and serialize to Parquet

--- src/processing/spark_processor.py
+++ src/processing/spark_processor.py
@@ -1,17 +1,48 @@
+import os
 import logging
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, split, trim
 
 def create_spark_session():
     """Initializes a local Spark Session for Data Lakehouse processing."""
     logging.info("Initializing local Spark Session...")
-
-    # configure Spark to use 8GB of RAM for local processing
-    # and set the default compression for our output Parquet files.
-    spark = SparkSession.builder \
+    return SparkSession.builder \
         .appName("HurricaneDataLake") \
         .config("spark.driver.memory", "8g") \
         .config("spark.sql.parquet.compression.codec", "snappy") \
         .getOrCreate()
 
-    logging.info("Spark Session successfully created.")
-    return spark
+def process_hurdat_data(spark, input_path, output_path):
+    """Processes structured HURDAT2 text data into a defined schema and Parquet files."""
+    logging.info("Starting Spark transformation on HURDAT2 data...")
+
+    try:
+        #  Read raw CSV-like text
+        df = spark.read.text(input_path)
+
+        #  Filter out header rows (Tracking rows always start with an 8-digit date YYYYMMDD)
+        tracking_df = df.filter(col("value").rlike("^[0-9]{8}"))
+
+        # Clean and Structure
+        # split the single comma-separated string into actual, usable columns
+        split_cols = split(tracking_df['value'], ',')
+
+        structured_df = tracking_df \
+            .withColumn("date", trim(split_cols.getItem(0))) \
+            .withColumn("time", trim(split_cols.getItem(1))) \
+            .withColumn("status", trim(split_cols.getItem(3))) \
+            .withColumn("latitude", trim(split_cols.getItem(4))) \
+            .withColumn("longitude", trim(split_cols.getItem(5))) \
+            .withColumn("max_wind_knots", trim(split_cols.getItem(6)).cast("integer")) \
+            .drop("value") # Drop the messy original column
+
+        # Save to Parquet
+        target_path = os.path.join(output_path, "hurdat_features.parquet")
+        structured_df.write.mode("overwrite").parquet(target_path)
+
+        logging.info(f"Successfully processed HURDAT2 data and saved Parquet to: {target_path}")
+        return True
+
+    except Exception as e:
+        logging.error(f"Spark processing failed: {e}")
+        return False
