069d6d361ed7b51ae5649ba6e19cd8bba0287227
Bryan-Julius <Bryanjulius77@gmail.com>
2026-04-05 00:29:21 -0400

    refactor: integrate Spark processing phase into pipeline orchestrator had error with phase 2 will debug later

--- src/main.py
+++ src/main.py
@@ -2,37 +2,42 @@ import os
 import yaml
 import logging
 from dotenv import load_dotenv
+
+# Import our custom modules
 from fetch.fetch_hurdat import download_hurdat_data
 from fetch.fetch_goes import download_sample_goes_data
+from processing.spark_processor import create_spark_session, process_hurdat_data
 
-# Set up basic logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def load_config(config_path: str = 'config/settings.yaml'):
+def load_config(config_path='config/settings.yaml'):
     with open(config_path, 'r') as f:
         return yaml.safe_load(f)
 
 def ensure_directories(config: dict):
-    # Check for directories
+    """Ensure storage directories exist."""
     os.makedirs(config['storage']['raw_data_path'], exist_ok=True)
     os.makedirs(config['storage']['processed_data_path'], exist_ok=True)
 
 def main():
-    logging.info("Starting Hurricane Big Data Pipeline")
+    logging.info("Starting Hurricane Big Data Pipeline...")
 
-    # Load Environment Variables and Config
+    # Load Configuration
     load_dotenv()
     config = load_config()
     ensure_directories(config)
 
     raw_path = config['storage']['raw_data_path']
+    processed_path = config['storage']['processed_data_path']
 
-    #  Fetch HURDAT2 Data
+
+    # First phase get data
+
+    logging.info(" Phase 1: Get data")
     hurdat_cfg = config['sources']['hurdat']
-    download_hurdat_data(hurdat_cfg['url'], raw_path, hurdat_cfg['filename'])
-
-    # Fetch GOES-16 Satellite Data
     goes_cfg = config['sources']['goes_s3']
+
+    download_hurdat_data(hurdat_cfg['url'], raw_path, hurdat_cfg['filename'])
     download_sample_goes_data(
         bucket_name=goes_cfg['bucket_name'],
         prefix=goes_cfg['product_prefix'],
@@ -40,7 +45,18 @@ def main():
         max_files=goes_cfg['max_files_to_download']
     )
 
-    logging.info("Pipeline Run Complete. Data is persisted in storage.")
+
+    # Second phase spark processing and data models
+
+    logging.info(" Phase 2: PROCESSING (SPARK)")
+    spark = create_spark_session()
+
+    hurdat_input = os.path.join(raw_path, hurdat_cfg['filename'])
+    process_hurdat_data(spark, hurdat_input, processed_path)
+
+    # Shut down the Spark cluster cleanly
+    spark.stop()
+    logging.info(" Pipeline Execution Complete. Data is processed and persisted in Data Lake.")
 
 if __name__ == "__main__":
     main()
